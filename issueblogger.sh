63a44cbac18c32623a249a16b03f2a101f896241
mcfrank <mcfrank@goldenticket.local>
2023-11-27 06:49:34 +0100

    changed system prompt and adding metadata to frotnmatter

--- issueblogger.sh
+++ issueblogger.sh
@@ -32,7 +32,7 @@ RESPONSE=$(curl -s "https://api.openai.com/v1/chat/completions" \
   "messages": [
     {
       "role": "system",
-      "content": "You are a helpful assistant. Reformat the following GitHub issue content into a concise markdown format suitable for about 4 tweets, focusing on how it will help the project succeed in helping users develop new tools."
+      "content": "You are a helpful assistant. Reformat the following GitHub issue content into a concise markdown format suitable for about 4 tweets, focusing on how it will help the project succeed in helping users develop new tools that specialize in improving developer experience. Your main goal is to have the highest possible information/token ratio possible."
     },
     {
       "role": "user",
@@ -43,5 +43,13 @@ RESPONSE=$(curl -s "https://api.openai.com/v1/chat/completions" \
 EOF
 )
 
-echo "$RESPONSE" > "blog/issue_${ISSUE_NUMBER}.md"
+CONTENT=$(echo "$RESPONSE" | jq -r '.choices[0].message.content')
+METADATA=$(echo "$RESPONSE" | jq '{id: .id, model: .model, created: .created}')
 
+# Write the metadata and content to a markdown file
+{
+  echo "---"
+  echo "$METADATA" | yq e -P -  # Convert JSON metadata to YAML
+  echo "---"
+  echo "$CONTENT"
+} > "blog/issue_${ISSUE_NUMBER}.md"
