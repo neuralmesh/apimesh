f67d561235770cfefcba71ee867585f3a0ee496b
mcfrank <mcfrank@goldenticket.local>
2023-11-28 07:30:00 +0100

    updated taskrouter

--- taskrouter.sh
+++ taskrouter.sh
@@ -1,45 +1,27 @@
 #!/bin/bash
 
-# Environment Variables
-OPENAI_API_KEY=$1
-GH_TOKEN=$2
-ISSUE_NUMBER=$3
-LABEL_NAME=$4
+# Debug: Echoing starting message
+echo "Starting script with LABEL_NAME: $4"
 
-function executeScript {
+# Associative array to map label names to script URLs
+declare -A scriptMap
+scriptMap["comment"]="https://raw.githubusercontent.com/neuralmesh/apimesh/main/issuecommenter.sh"
+scriptMap["blog"]="https://raw.githubusercontent.com/neuralmesh/apimesh/main/issueblogger.sh"
+scriptMap["taskrouter"]="https://raw.githubusercontent.com/neuralmesh/apimesh/main/taskrouter.sh"
+
+# Function to execute a script from a URL
+executeScript() {
     local script_url=$1
-    shift
-    local script_name=$(basename "$script_url")
-
-    # Fetch and execute the script with any additional arguments
-    curl -s "$script_url" > "$script_name" && chmod +x "$script_name" && ./"$script_name" "$@"
+    echo "Executing script from: $script_url"
+    bash <(curl -s "$script_url") "${@:2}"
 }
 
-function handleBlog {
-    executeScript "https://raw.githubusercontent.com/neuralmesh/apimesh/main/issueblogger.sh" "$ISSUE_NUMBER"
-}
-
-function handleTaskRouter {
-    executeScript "https://raw.githubusercontent.com/neuralmesh/apimesh/main/taskrouter.sh"
-}
-
-function handleComment {
-    executeScript "https://raw.githubusercontent.com/neuralmesh/apimesh/main/issuecommenter.sh" "$OPENAI_API_KEY" "$GH_TOKEN" "$ISSUE_NUMBER"
-}
-
-# Main
-case "$LABEL_NAME" in
-  "comment")
-    handleComment
-    ;;
-  "blog")
-    handleBlog
-    ;;
-  "taskrouter")
-    handleTaskRouter
-    ;;
-  *)
-    echo "No matching handler for label: $LABEL_NAME"
-    ;;
-esac
+# Main logic
+if [[ -n "${scriptMap[$4]}" ]]; then
+    # Debug: Echoing which script will be executed
+    echo "Handling label: $4"
+    executeScript "${scriptMap[$4]}" "$@"
+else
+    echo "No matching handler for label: $4"
+fi
 
