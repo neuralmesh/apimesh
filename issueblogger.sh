a6945eaf3cbc5e2fd2a3652e1d9b25233df022c3
mcfrank <mcfrank@goldenticket.local>
2023-11-29 06:00:37 +0100

    updated issueblogger

--- issueblogger.sh
+++ issueblogger.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
-# Check if the issue number is provided
-if [ -z "$1" ]
-then
-  echo "Error: No issue number provided." >&2
-  exit 1
-fi
+# Exit if any command fails
+set -e
 
-# Assign the issue number to a variable
-ISSUE_NUMBER=$1
+# Check required environment variables
+required_env_vars=("GH_TOKEN" "OPENAI_API_KEY" "ISSUE_NUMBER")
+for var in "${required_env_vars[@]}"; do
+    if [ -z "${!var}" ]; then 
+        echo "Error: $var environment variable is not set."
+        exit 1
+    fi
+done
 
-mkdir -p blog
-
-# Fetch the issue data using GitHub CLI and store it in a variable
-ISSUE_CONTENT=$(gh issue view "$ISSUE_NUMBER" --json title,body,comments -t '{{.title}}|{{.body}}{{range .comments}}|{{.body}}{{end}}')
-
-# Ensure your OpenAI API key is set in the environment variable
-if [ -z "$OPENAI_API_KEY" ]
-then
-  echo "Error: OPENAI_API_KEY is not set." >&2
-  exit 1
-fi
+ISSUE_CONTENT=$(bash ./issuefetcher.sh)
 
 # Make a request to the OpenAI API using curl
 RESPONSE=$(curl -s "https://api.openai.com/v1/chat/completions" \
@@ -32,7 +24,7 @@ RESPONSE=$(curl -s "https://api.openai.com/v1/chat/completions" \
   "messages": [
     {
       "role": "system",
-      "content": "You are a helpful assistant. Reformat the following GitHub issue content into a concise markdown format suitable for about 4 tweets, focusing on how it will help the project succeed in helping users develop new tools that specialize in improving developer experience. Your main goal is to have the highest possible information/token ratio possible. And make sure you respond in a nice and professional concise neutral tone. Write in information dense paragraphs and really take a step back to think about how to reformulate what has actually been discussed to make it clear for everyone that reads it. Perhaps do it in three levels of abstraction (but dont explicitly state that) use this general formula: 1. how does this improve everyones life, or how does this affect me personally (positively of course) 2. why do you think it is required to make sure the project can continue and we wont lose our jobs 3. get into some implementation details on how this could be achieved, give insights as to whats already possible with large language models and indicate that they are personal assistants. Okay i think you get my point. now really reflect on that and turn it into a markdown formatted structure to be rendered beautifully while maintaining professionalism and concise neutral language."
+      "content": "You are a helpful assistant. Reformat the following issue and its discussion content into a concise markdown format suitable for documenting every concept that was mentioned in around 4 tweets of length, focusing on how it directly impacts the project succeed in helping users develop new tools that specialize in improving developer experience. Your main goal is to have the highest possible information/token ratio possible. And make sure you respond in a nice concise neutral tone. Write in information dense paragraphs and really take a step back to think about how to reformulate what has actually been discussed to make it clear for everyone that reads it. Perhaps do it in three levels of abstraction (but dont explicitly state that) use this general formula: 1. how does this improve everyones life, or how does this affect me personally (positively of course) 2. why do you think it is required to make sure the project can continue and we wont lose our jobs 3. get into some implementation details on how this could be achieved, give insights as to whats already possible with large language models and indicate that they are personal assistants. Okay i think you get my point. now really reflect on that and turn it into a markdown formatted structure to be rendered beautifully while maintaining professionalism and concise neutral language. You can also add a post scriptum section on serious concerns that need to be dealt with."
     },
     {
       "role": "user",
@@ -53,3 +45,22 @@ METADATA=$(echo "$RESPONSE" | jq '{id: .id, model: .model, created: .created}')
   echo "---"
   echo "$CONTENT"
 } > "blog/issue_${ISSUE_NUMBER}.md"
+
+# Create a new branch and switch to it
+gh repo create-branch "issue-${ISSUE_NUMBER}-branch"
+
+# Add the new markdown file
+git add "blog/issue_${ISSUE_NUMBER}.md"
+
+# Commit the changes
+git commit -m "Add blog post for issue $ISSUE_NUMBER"
+
+# Push the branch to the remote repository
+gh repo view --web --branch "issue-${ISSUE_NUMBER}-branch"
+
+# Create a pull request
+gh pr create --title "Blog Post for Issue $ISSUE_NUMBER" \
+             --body "Adding a blog post for issue $ISSUE_NUMBER." \
+             --base main \
+             --head "issue-${ISSUE_NUMBER}-branch"
+
