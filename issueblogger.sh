13b1eb47bc149666991896914e7d568d2ea1197b
GitHub Action <action@github.com>
2023-11-30 07:43:20 +0100

    refactored issueblogger to work with new llmprompter and pullrequester

--- issueblogger.sh
+++ issueblogger.sh
@@ -8,17 +8,7 @@ bash envtester.sh OPENAI_API_KEY ISSUE_NUMBER MODEL_LLM
 
 ISSUE_CONTENT=$(bash ./issuefetcher.sh)
 
-# Make a request to the OpenAI API using curl
-RESPONSE=$(curl -s "https://api.openai.com/v1/chat/completions" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d @- <<EOF
-{
-  "model": "$MODEL_LLM",
-  "messages": [
-    {
-      "role": "system",
-      "content": "Your task is to convert a discussion into a Markdown document, with an emphasis on effectively encapsulating all critical concepts. Your writing should be neutral and concise, prioritizing clarity, relevance, and a high information-to-token ratio.
+SYSTEM_MESSAGE="Your task is to convert a discussion into a Markdown document, with an emphasis on effectively encapsulating all critical concepts. Your writing should be neutral and concise, prioritizing clarity, relevance, and a high information-to-token ratio.
 
 Overview: Start by outlining the overall impact of the issue on the audience, highlighting positive outcomes and personal relevance.
 
@@ -31,32 +21,18 @@ Guidelines: Strive for an information-rich content approach, steering clear of u
 Postscriptum: End with a segment addressing any significant concerns or challenges that merit attention.
 
 The goal is to craft a clear, professional, and well-organized Markdown document, enabling readers to quickly comprehend the core of the discussion. Include code examples or intriguing concepts where applicable to enhance practical understanding and application."
-    },
-    {
-      "role": "user",
-      "content": "$ISSUE_CONTENT"
-    }
-  ]
-}
-EOF
-)
 
-CONTENT=$(echo "$RESPONSE" | jq -r '.choices[0].message.content')
-METADATA=$(echo "$RESPONSE" | jq '{id: .id, model: .model, created: .created}')
+USER_MESSAGE="$ISSUE_CONTENT"
 
-# Write the metadata and content to a markdown file
-{
-  echo "---"
-  echo "$METADATA" | yq e -P -  # Convert JSON metadata to YAML
-  echo "Issue $ISSUE_NUMBER"
-  echo "---"
-  echo "$CONTENT"
-} > "blog/issue_${ISSUE_NUMBER}.md"
+RESPONSE=$(bash ./llmprompter.sh "$SYSTEM_MESSAGE" "$ISSUE_CONTENT")
+
+echo "$RESPONSE" > "blog/issue_${ISSUE_NUMBER}.md"
 
 # Set and export BRANCH_NAME as an environment variable
-export BRANCH_NAME="blog-issue-${ISSUE_NUMBER}"
+BRANCH_NAME="blog-issue-${ISSUE_NUMBER}"
+REPOSITORY="neuralmesh/apimesh"
 
 gh issue edit "$ISSUE_NUMBER" --remove-label "$LABEL_NAME"
 
-bash ./pullrequester.sh
+bash ./pullrequester.sh $BRANCH_NAME $REPOSITORY
 
